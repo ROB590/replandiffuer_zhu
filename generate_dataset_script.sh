1a52fb9006bd53ce86fcb9edfbd7aaf76051eb14
chenhzhu <chenhzhu@umich.edu>
2025-05-25 00:10:09 -0700

    Create generate_dataset_script.sh

---
+++ generate_dataset_script.sh
@@ -0,0 +1,7 @@
+python rlbench/dataset_generator.py \
+    --tasks close_box \
+    --variations 0 \
+    --renderer opengl3 \
+    --episodes_per_task 100 \
+    --processes 1 \
+    --save_path RLBench_data//clip
